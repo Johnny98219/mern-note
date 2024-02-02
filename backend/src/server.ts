584b48f7ce4b5bb22e0e965d27e409d1db5e4f59
Johnny98219 <johnnybrown98219>
2024-02-02 14:35:55 -0600

    add MongoDB connect and envalid .env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,17 @@
-import express from "express";
-const app = express();
-const port = 5000;
+import app from "./app";
+import env from "./utill/validateEnv"
+import mongoose from "mongoose";
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
 
-app.listen(port!, () => {
-    console.log("Server running on port: " + port);
-});
+
+const port = env.PORT ? env.PORT : 5000;
+
+mongoose.connect(env.MONGODB_URL)
+    .then(() => {
+        console.log("Mongoose connected");
+        app.listen(port, () => {
+            console.log("Server running on port: " + port);
+            
+        });
+    })
+    .catch(console.error);
